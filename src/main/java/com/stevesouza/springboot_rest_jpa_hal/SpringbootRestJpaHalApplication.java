301f6d941f6356aba391cbda2b0867e0ae54cb25
Steve Souza <steve@stevesouza.com>
2018-06-20 14:17:12 -0400

    Initial check in of spring boot generated code

---
+++ src/main/java/com/stevesouza/springboot_rest_jpa_hal/SpringbootRestJpaHalApplication.java
@@ -0,0 +1,12 @@
+package com.stevesouza.springboot_rest_jpa_hal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestJpaHalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestJpaHalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/stevesouza/springboot_rest_jpa_hal/SpringbootRestJpaHalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stevesouza.springboot_rest_jpa_hal;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestJpaHalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
