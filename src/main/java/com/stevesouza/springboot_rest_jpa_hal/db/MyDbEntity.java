1b455c75055e58f195e553f20561931e52dca989
Steve Souza <steve@stevesouza.com>
2018-06-28 09:24:36 -0400

    tested various javax.persistence validator annotations
    @Min, @Max, @Size

--- src/main/java/com/stevesouza/springboot_rest_jpa_hal/db/MyDbEntity.java
+++ src/main/java/com/stevesouza/springboot_rest_jpa_hal/db/MyDbEntity.java
@@ -4,7 +4,18 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+/**
+ * validation information https://spring.io/guides/gs/validating-form-input/
+ * 
+ * @author stevesouza
+ *
+ */
 
 @Entity
 public class MyDbEntity {
@@ -12,7 +23,26 @@ public class MyDbEntity {
 	@Id
 	@GeneratedValue // defaults to AUTO which picks the best strategy for the given db
 	private long id;
+	private String strField1;
+	private String strField2;
+	// override column name
+	@Column(name="strField3")
+	@NotNull
+	@Size(min=2, message="Name should have atleast 2 characters")
+	private String str;
 	
+	// note this will error out if value isn't between 0 and 100 however it will 
+	// only send an 500 error.  Further work must be done to send a more specific error
+	@Min(0)
+	@Max(100)
+	int age;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	public String getStrField1() {
 		return strField1;
 	}
@@ -26,11 +56,6 @@ public class MyDbEntity {
 		this.strField2 = strField2;
 	}
 	
-	private String strField1;
-	private String strField2;
-	// override column name
-	@Column(name="strField3")
-	private String str;
 	public String getStrField3() {
 		return str;
 	}
